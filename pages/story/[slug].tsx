8a883dfb761447b181182775c9c49e900c15f947
Aremu Smog <aremuoluwagbamila@gmail.com>
2023-12-26 18:35:37 +0100

    chore: disable data fetching for page

--- pages/story/[slug].tsx
+++ pages/story/[slug].tsx
@@ -171,15 +171,15 @@ export const getStaticProps: GetStaticProps = async ({
 	preview = false,
 	previewData,
 }) => {
-	const data = await getPostAndMorePosts(params?.slug, preview, previewData)
+	// const data = await getPostAndMorePosts(params?.slug, preview, previewData)
 
-	console.log({ data })
+	// console.log({ data })
 
 	return {
 		props: {
 			preview,
-			post: data.post ?? null,
-			posts: data.posts ?? null,
+			post:  null,
+			posts: null,
 		},
 		// revalidate: 10,
 	}
