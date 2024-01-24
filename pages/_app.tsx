d18bd4cd081f1c4f66b6c3879179f1d640dd1271
Dev Jo <idowuayanfeoluwa@gmail.com>
2024-01-24 02:27:43 +0100

    feat: added google tag to website

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,43 +1,47 @@
-import { AppProps } from "next/app"
-import "../styles/index.css"
-import localFont from "next/font/local"
-import "react-toastify/dist/ReactToastify.css"
-import SubscribersContextProvider from "context/SubscribersContext"
+import { AppProps } from "next/app";
+import "../styles/index.css";
+import localFont from "next/font/local";
+import "react-toastify/dist/ReactToastify.css";
+import SubscribersContextProvider from "context/SubscribersContext";
+import { GoogleAnalytics } from "../components";
 
 const generalSans = localFont({
-	src: [
-		{
-			path: "../fonts/GeneralSans-Regular.otf",
-			weight: "400",
-			style: "normal",
-		},
-	],
-	variable: "--font-secondary",
-})
+  src: [
+    {
+      path: "../fonts/GeneralSans-Regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  variable: "--font-secondary",
+});
 const clashDisplay = localFont({
-	src: [
-		{
-			path: "../fonts/ClashDisplay-Regular.otf",
-			weight: "400",
-			style: "normal",
-		},
-		{
-			path: "../fonts/ClashDisplay-Medium.otf",
-			weight: "500",
-			style: "normal",
-		},
-	],
-	variable: "--font-primary",
-})
+  src: [
+    {
+      path: "../fonts/ClashDisplay-Regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../fonts/ClashDisplay-Medium.otf",
+      weight: "500",
+      style: "normal",
+    },
+  ],
+  variable: "--font-primary",
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return (
-		<main className={`${clashDisplay.variable} ${generalSans.variable}  `}>
-			<SubscribersContextProvider>
-				<Component {...pageProps} />
-			</SubscribersContextProvider>
-		</main>
-	)
+  const googleAnalyticsId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
+
+  return (
+    <main className={`${clashDisplay.variable} ${generalSans.variable}  `}>
+      {googleAnalyticsId ? <GoogleAnalytics ga_id={googleAnalyticsId} /> : null}
+      <SubscribersContextProvider>
+        <Component {...pageProps} />
+      </SubscribersContextProvider>
+    </main>
+  );
 }
 
-export default MyApp
+export default MyApp;
