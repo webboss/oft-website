483af1aee41114966ea211478a3bf08e505d309d
Dev Jo <idowuayanfeoluwa@gmail.com>
2024-04-03 08:40:25 +0100

    feat: test input component handles user input

--- components/input/input.test.tsx
+++ components/input/input.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { describe, expect, test } from "vitest";
 import { Input } from "./input";
+import userEvent from "@testing-library/user-event";
 
 describe("Input Component", () => {
   test("renders input without label", () => {
@@ -17,5 +18,16 @@ describe("Input Component", () => {
     const inputElement = screen.getByLabelText("Test Label");
 
     expect(inputElement).toBeInTheDocument();
+  });
+
+  test("handles user input", async () => {
+    const user = userEvent.setup();
+
+    render(<Input label="Username" />);
+
+    const inputElement = screen.getByLabelText("Username") as HTMLInputElement;
+
+    await user.type(inputElement, "John Doe");
+    expect(inputElement.value).toBe("John Doe");
   });
 });
