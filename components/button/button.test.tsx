681f9ce56688c6fe2ed1c6808257168633c6cf6f
Dev Jo <idowuayanfeoluwa@gmail.com>
2024-04-08 09:33:18 +0100

    fix: button test

--- components/button/button.test.tsx
+++ components/button/button.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { describe, expect, test, vi } from "vitest";
-import { Button } from "./";
+import { Button } from "../button";
 
 describe("Button component", () => {
   test("renders button", () => {
