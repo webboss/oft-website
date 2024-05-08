b5ced79bc9ec2dcf9a8be4fde42178869b601d64
Dev Jo <idowuayanfeoluwa@gmail.com>
2024-05-08 18:21:26 +0100

    feat: test navbar handles menu toggle

--- components/layout/navbar/navbar.test.tsx
+++ components/layout/navbar/navbar.test.tsx
@@ -1,7 +1,6 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { describe, expect, test } from "vitest";
-import MENU_LINKS from "config/menu.json";
-import { Menu } from "./menu";
 import { NavBar } from "./navbar";
 
 describe("Navbar", () => {
@@ -14,5 +13,22 @@ describe("Navbar", () => {
     expect(menuIcon.tagName).eq("svg");
     expect(menuIcon).toBeInTheDocument();
     expect(hamburgerButton).toContain(menuIcon);
+  });
+
+  test("handles menu toggle", async () => {
+    const user = userEvent.setup();
+    render(<NavBar />);
+
+    const header = screen.getByRole("banner");
+    const toggleButton = screen.getByRole("button");
+
+    expect(header).not.toHaveClass("h-full");
+
+    await user.click(toggleButton);
+
+    const closeIcon = await screen.findByTitle("Close");
+
+    expect(closeIcon).toBeInTheDocument();
+    expect(header).toHaveClass("h-full");
   });
 });
