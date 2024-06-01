c02f3ccd56a5cc144ed861d1cb134b48df3494fb
Dev Jo <idowuayanfeoluwa@gmail.com>
2024-06-01 16:52:02 +0100

    feat: test Date component has the correct dateTime attribute

--- components/date.test.tsx
+++ components/date.test.tsx
@@ -14,4 +14,16 @@ describe("Date component", () => {
 
     expect(dateElement).toBeInTheDocument();
   });
+
+  test("has the correct dateTime attribute", () => {
+    const dateString = "2024-05-31";
+
+    render(<Date dateString={dateString} />);
+
+    const expectedFormatedDate = "May 31, 2024";
+
+    const dateElement = screen.getByText(expectedFormatedDate);
+
+    expect(dateElement).toHaveAttribute("dateTime", dateString);
+  });
 });
